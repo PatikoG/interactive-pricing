ee479fb0f4aac028190fbefe5d9831a018f7e659
patiko <gharibashvilip@gmail.com>
2022-08-05 14:20:20 +0400

    third commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
-const range = document.getElementById("rangeValue").value;
+const range = document.getElementById("rangeValue");
 const pageview = document.getElementById("pageViewers");
 const cost = document.getElementById("costOfTrial");
 const progressbar = document.getElementById("progress");
+const dollar = document.getElementById("dollarcost");
+const checked = document.getElementById("checkbilling");
 
 
 function handleInput(inputD){
     progressbar.style.width = (inputD.value / 32 * inputD.clientWidth) + "px";
     
-    console.log("es aris calculated sigane", inputD.value / 100 * inputD.clientWidth);
+    
+    if(checked.checked){    
+        dollar.innerHTML  =  "$" + (inputD.value * 12) * (75/100) + ".00";
+        pageview.innerHTML = (inputD.value / 32 * 200) * 2 + "K " + "PAGEVIEWS";
+    } else {
+        dollar.innerHTML  =  "$" + inputD.value + ".00";
+        pageview.innerHTML = (inputD.value / 32 * 200) + "K " + "PAGEVIEWS";
+    }
 }
 
-
+function changevalue(inputChange){
+    if(inputChange.checked){    
+            dollar.innerHTML  =  "$" + (range.value * 12) * (75/100) + ".00";
+            pageview.innerHTML = (range.value / 32 * 200) * 2 + "K " + "PAGEVIEWS";
+    } else {
+            dollar.innerHTML  =  "$" + range.value + ".00";
+            pageview.innerHTML = (range.value / 32 * 200) + "K " + "PAGEVIEWS";
+        }
+}
